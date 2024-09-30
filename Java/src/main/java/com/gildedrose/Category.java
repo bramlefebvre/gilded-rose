cd61e7ec27856f596b2e02804d5af79db4843085
Bram Lefebvre <bramlefebvre@gmail.com>
2024-09-30 15:09:56 +0200

    add @Override annotation

--- Java/src/main/java/com/gildedrose/Category.java
+++ Java/src/main/java/com/gildedrose/Category.java
@@ -3,26 +3,31 @@ package com.gildedrose;
 public enum Category {
 
     NORMAL {
+        @Override
         void updateQuality(QualityUpdater qualityUpdater, Item item) {
             qualityUpdater.updateQualityNormal(item);
         }
     },
     AGING {
+        @Override
         void updateQuality(QualityUpdater qualityUpdater, Item item) {
             qualityUpdater.updateQualityAging(item);
         }
     },
     LEGENDARY{
+        @Override
         void updateQuality(QualityUpdater qualityUpdater, Item item) {
             qualityUpdater.updateQualityLegendary(item);
         }
     },
     CONCERT_TICKET {
+        @Override
         void updateQuality(QualityUpdater qualityUpdater, Item item) {
             qualityUpdater.updateQualityConcertTicket(item);
         }
     },
     CONJURED {
+        @Override
         void updateQuality(QualityUpdater qualityUpdater, Item item) {
             qualityUpdater.updateQualityConjured(item);
         }
